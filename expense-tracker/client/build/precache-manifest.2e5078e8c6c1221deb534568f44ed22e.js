afc9690387d7f3224684b3827e2474de83330ff0
Nikunj Sharma <nikunj.sharma251@gmail.com>
2021-04-25 17:37:12 +0530

    Init

---
+++ expense-tracker/client/build/precache-manifest.2e5078e8c6c1221deb534568f44ed22e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a137a2224b66d94f49490a828c90c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd4bdfa2aa354ae8cee9",
+    "url": "/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd4bdfa2aa354ae8cee9",
+    "url": "/static/js/main.3e1e7911.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
